1da0a3a519ec544aca5fd210a89b3cf497a2fbde
majd-mora <majedmoura99@gmail.com>
2022-04-25 17:56:48 +0300

    fixing gender

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* functionalities */
 
-let sortDirection = false;
 //what does let do
 let personData = [
     {
@@ -35,86 +34,47 @@ personData.forEach((item, i) => {
 
 console.log(personData);
 
-//what do you call this function inside the sort?
-//explain callback functions
-personData.sort(function (a, b) {
-    const dateA = new Date(a.date), dateB = new Date(b.date)
-    return dateA - dateB
-});
 
 
-// W3Schools Read again to understand sorting fname and lname
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("myTable2");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
+
+
+    let data = [];
+
+    const addUser = (e) => {
+        e.preventDefault();
+        let User = {
+            firstName: document.getElementById('fname').value,
+            lastName: document.getElementById('lname').value,
+            gender: document.querySelector('input[name="gender"]:checked').value,
+            date: document.getElementById('birthday').value,
         }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
+        console.log(personData);
+        personData.push(User);
+        loadTableData(personData);
+        document.forms[0].reset();
+        e.preventDefault();
     }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
+    document.getElementById('submit').addEventListener('click', addUser);
 
-var data = [];
-document.querySelector('form button').addEventListener('click', function(event) {
-    
-    var inputs = document.querySelectorAll('form input');
-    var newPerson = {};
-    for (var i = 0; i < inputs.length; i++) {
-        newPerson[inputs[i].name] = inputs[i].value;
-        inputs[i].value = '';
-    }
-    console.log(personData)
-    personData.push(newPerson);
-    loadTableData(personData);
-    event.preventDefault();
-}, false);
 
-function loadTableData(personData) {
+
+
+// var data = [];
+// document.querySelector('form button').addEventListener('click', function(event) {
+//     var inputs = document.querySelectorAll('form input');
+//     var newPerson = {};
+//     for (var i = 0; i < inputs.length; i++) {
+//         newPerson[inputs[i].name] = inputs[i].value;
+//         inputs[i].value = '';
+//         const selectedGender = document.querySelector('input[name="gender"]:checked').value;
+//     }
+//     console.log(personData)
+//     personData.push(newPerson);
+//     loadTableData(personData);
+//     event.preventDefault();
+// }, false);
+
+function loadTableData(data) {
     const tableBody = document.getElementById('tableData');
     let dataHtml = '';
 
@@ -129,17 +89,5 @@ function loadTableData(personData) {
     tableBody.innerHTML = dataHtml;
 }
 
-// sorting dates
-function sortColumn(columnName) {
-    const dataType = typeof personData[0][columnName];
-    sortDirection = !sortDirection;
-
-    switch(dataType) {
-        case 'number':
-        sortNumberColumn(sortDirection, columnName);
-        break;
-    }
-    loadTableData(personData)
-}
 
 loadTableData(personData);
